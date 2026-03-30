d19dab147374b3e7b3f28730038a3b6dfe46bac9
Onja <rajohnarson01@gmail.com>
2026-03-30 11:14:45 -0700

    React Native: logging interface test

--- react_native_project/app/index.jsx
+++ react_native_project/app/index.jsx
@@ -1,14 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
-import "../styles/global.css" 
+import { Image, StyleSheet, Text, View } from "react-native";
+import Logo from "../assets/img/logo.png";
+import "../styles/global.css";
+import { Ionicons } from "@expo/vector-icons";
 
 const Home = () => {
   return (
-    <View className="h-full">
-      <Text>Home</Text>
+    <View className="h-full flex items-center bg-bank-01 justify-center gap-20">
+      <View className="h-52 w-52 bg-white p-6 rounded-md">
+        <Image source={Logo} className="h-full w-full" />
+      </View>
+      <View className="w-fit flex items-center gap-10">
+        <Text className="text-bank-05">Welcome to Bank Loan Management app</Text>
+        <View className="w-[300px] flex items-center bg-bank-04 py-4 rounded-lg">
+          <Text className="text-bank-01 flex gap-2">
+            Getting Started <Ionicons name="arrow-forward" />
+          </Text>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Home
-
-const styles = StyleSheet.create({})
+export default Home;
--- react_native_project/tailwind.config.js
+++ react_native_project/tailwind.config.js
@@ -1,10 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}",  "./app/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,jsx,ts,tsx}",
+  ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //Enchanted forest
+        'bank-02': '#ccfccb',
+        'bank-01': '#f1fffa',
+        'bank-03': '#96e6b3',
+        'bank-04': '#568259',
+        'bank-05': '#464e47',
+
+        //Forest green
+        'bank-1': '#dce1de',
+        'bank-2': '#9cc5a1',
+        'bank-3': '#49a078',
+        'bank-4': '#216869',
+        'bank-5': '#1f2421',
+
+        //Sky blue freshness
+        'bank-11': "#ebf2fa",
+        'bank-12': "#427aa1",
+        'bank-13': "#05668d",
+        'bank-14': "#a5be00",
+        'bank-15': "#679436",
+      },
+    },
   },
   plugins: [],
-}
+};
 
